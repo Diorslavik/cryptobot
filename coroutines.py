3cdf0ba78370f6b66adf71d923567818e3afab65
Ivashkaization <ivashov99@gmail.com>
2017-11-22 17:03:49 +0400

    Added models

--- coroutines.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-
-@asyncio.coroutine
-def exchange_coroutine(exchange, method='trades', sleep_time=1, limit=None):
-    while True:
-        yield from asyncio.sleep(sleep_time)
-        print(exchange.execute_method(method, limit))
