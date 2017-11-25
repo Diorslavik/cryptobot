f9db45aa08674293c7a602208afcd286353afbc7
rumiljonov <rustamkom97@gmail.com>
2017-11-25 23:24:01 +0300

    Fix

--- orderbook.py
+++ orderbook.py
@@ -49,11 +49,6 @@ class BitfinexOrderBook(OrderBook):
         self.bids = [order for order in orders if order.amount > 0]
         self.asks = [order for order in orders if order.amount < 0]
 
-    def __init__(self, orders=list()):
-        super(BitfinexOrderBook, self).__init__(self)
-        self.bids = [order for order in orders if order.amount > 0]
-        self.asks = [order for order in orders if order.amount < 0]
-
     def update(self, order):
         if order.price == 0:
             if order.amount > 0:
